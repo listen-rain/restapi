7d177ceb2ef2d0848d1d7d344cb93534c3882a1c
zhufengwei <zhufengwei@100tal.com>
2019-01-04 14:03:09 +0800

    update addlog

--- src/Restapi.php
+++ src/Restapi.php
@@ -2,19 +2,26 @@
 
 namespace Listen\Restapi;
 
-
 use Exception;
 use GuzzleHttp\Client;
 use GuzzleHttp\Exception\RequestException;
 use GuzzleHttp\Psr7\Request;
+use GuzzleHttp\Psr7\Response;
+use http\Url;
 use Illuminate\Contracts\Config\Repository;
 use Monolog\Formatter\LineFormatter;
 use Monolog\Handler\RotatingFileHandler;
 use Monolog\Logger;
+use phpDocumentor\Reflection\Types\Integer;
 use Psr\Http\Message\ResponseInterface;
 
 class Restapi
 {
+    /**
+     * Restapi constructor.
+     *
+     * @param \Illuminate\Contracts\Config\Repository $config
+     */
     public function __construct(Repository $config)
     {
         $this->config = $config;
@@ -31,7 +38,17 @@ class Restapi
             ]);
     }
 
-    protected function addSign($params, $secret)
+    /**
+     * @date   2019/1/4
+     * @author <zhufengwei@100tal.com>
+     *
+     * @param array $params
+     * @param       $secret
+     *
+     * @return array
+     */
+    protected function addSign(array $params, $secret)
+    : array
     {
         if ($secret && !isset($params['sign'])) {
             $params['sign'] = $this->getSign($params, $secret);
@@ -40,67 +57,36 @@ class Restapi
         return $params;
     }
 
-    protected function getSign($params, $secret)
+    /**
+     * @date   2019/1/4
+     * @author <zhufengwei@100tal.com>
+     *
+     * @param string $module
+     * @param string $uri
+     * @param string $action
+     *
+     * @return object
+     */
+    protected function makeRequest(string $module, string $uri, string $action)
+    : Request
     {
-        ksort($params);
-        $paramStr = http_build_query($params, null, ini_get('arg_separator.output'), PHP_QUERY_RFC3986);
-        return md5(md5($paramStr) . $secret);
-    }
-
-    public function get($module, $uri, $params, $headers = [], $action = 'GET')
-    {
-        $options = [
-            'query'   => $params,
-            'headers' => $headers
-        ];
-
-        $result = $this->client->send(
-            $this->makeRequest($module, $uri, $params, $headers, $action),
-            $options
-        );
-
-        return $this->getResponse($module, $params, $result, $uri);
-    }
-
-    public function post($module, $uri, $params, $headers = [], $action = 'POST')
-    {
-        $options = [
-            'form_params' => $params,
-            'headers'     => $headers
-        ];
-
-        $result = $this->client->send(
-            $this->makeRequest($module, $uri, $params, $headers, $action),
-            $options
-        );
-
-        return $this->getResponse($module, $params, $result, $uri);
-    }
-
-    public function multipart($module, $uri, $params, $headers = [], $action = 'POST')
-    {
-        $options = [
-            'multipart' => $params,
-            'headers'   => $headers
-        ];
-
-        $result = $this->client->send(
-            $this->makeRequest($module, $uri, $params, $headers, $action),
-            $options
-        );
-
-        return $this->getResponse($module, $params, $result, $uri);
-    }
-
-    protected function makeRequest($module, $uri, $params, $headers = [], $action)
-    {
-        $params   = $this->addSign($params, $this->config->get('restapi.' . $module . '.secret'));
         $base_uri = $this->getBaseUri($module);
 
         return new Request($action, $base_uri . $uri);
     }
 
-    protected function getResponse($module, $params, $response, $uri)
+    /**
+     * @date   2019/1/4
+     * @author <zhufengwei@100tal.com>
+     *
+     * @param string $module
+     * @param array  $params
+     * @param string $response
+     * @param string $uri
+     *
+     * @return bool|mixed
+     */
+    protected function getResponse(string $module, array $params, Response $response, string $uri)
     {
         if (!$response) {
             $this->addlog($module, $uri, $params, 'RESQUST_ERROR', 404);
@@ -108,15 +94,12 @@ class Restapi
         }
 
         $code = $response->getStatusCode();
-
         if ($code != 200) {
             $this->addlog($module, $uri, $params, '', $code);
             return false;
         }
 
-        $stringBody = strval($response->getBody());
-        $result     = json_decode($stringBody, true);
-
+        $result = json_decode(strval($response->getBody()), true);
         if (!$result) {
             $this->addlog($module, $uri, $params, $stringBody, $code);
             return false;
@@ -126,7 +109,107 @@ class Restapi
         return $result;
     }
 
-    public function mget($apis)
+    /**
+     * @date   2019/1/4
+     * @author <zhufengwei@100tal.com>
+     *
+     * @param array  $params
+     * @param string $secret
+     *
+     * @return string
+     */
+    protected function getSign(array $params, string $secret)
+    : string
+    {
+        ksort($params);
+        $paramStr = http_build_query($params, null, ini_get('arg_separator.output'), PHP_QUERY_RFC3986);
+        return md5(md5($paramStr) . $secret);
+    }
+
+    /**
+     * @date   2019/1/4
+     * @author <zhufengwei@100tal.com>
+     *
+     * @param string $module
+     * @param string $uri
+     * @param array  $params
+     * @param array  $headers
+     * @param string $action
+     *
+     * @return bool|mixed
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function get(string $module, string $uri, array $params, array $headers = [], string $action = 'GET')
+    {
+        $options = [
+            'query'   => $this->addSign($params, $this->config->get('restapi.' . $module . '.secret')),
+            'headers' => $headers
+        ];
+
+        $result = $this->client->send($this->makeRequest($module, $uri, $action), $options);
+        return $this->getResponse($module, $params, $result, $uri);
+    }
+
+    /**
+     * @date   2019/1/4
+     * @author <zhufengwei@100tal.com>
+     *
+     * @param string $module
+     * @param string $uri
+     * @param array  $params
+     * @param array  $headers
+     * @param string $action
+     *
+     * @return bool|mixed
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function post(string $module, string $uri, array $params, array $headers = [], string $action = 'POST')
+    {
+        $options = [
+            'form_params' => $this->addSign($params, $this->config->get('restapi.' . $module . '.secret')),
+            'headers'     => $headers
+        ];
+
+        $result = $this->client->send($this->makeRequest($module, $uri, $action), $options);
+
+        return $this->getResponse($module, $params, $result, $uri);
+    }
+
+    /**
+     * @date   2019/1/4
+     * @author <zhufengwei@100tal.com>
+     *
+     * @param string $module
+     * @param string $uri
+     * @param array  $params
+     * @param array  $headers
+     * @param string $action
+     *
+     * @return bool|mixed
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function multipart(string $module, string $uri, array $params, array $headers = [], string $action = 'POST')
+    {
+        $options = [
+            'multipart' => $this->addSign($params, $this->config->get('restapi.' . $module . '.secret')),
+            'headers'   => $headers
+        ];
+
+        $result = $this->client->send($this->makeRequest($module, $uri, $action), $options);
+        return $this->getResponse($module, $params, $result, $uri);
+    }
+
+    /**
+     * @date   2019/1/4
+     * @author <zhufengwei@100tal.com>
+     *
+     * @param array $apis
+     *
+     * @return array
+     * @throws \Throwable
+     */
+    public function mget(array $apis)
+    : array
     {
         if (!is_array($apis)) {
             return [];
@@ -169,12 +252,31 @@ class Restapi
         return $return;
     }
 
-    protected function getBaseUri($module)
+    /**
+     * @date   2019/1/4
+     * @author <zhufengwei@100tal.com>
+     *
+     * @param string $module
+     *
+     * @return string
+     */
+    protected function getBaseUri(string $module)
+    : string
     {
         return $this->config->get('restapi.' . $module . '.base_uri');
     }
 
-    public function addlog($module, $uri, $request, $response, $code)
+    /**
+     * @date   2019/1/4
+     * @author <zhufengwei@100tal.com>
+     *
+     * @param string $module
+     * @param string $uri
+     * @param array  $request
+     * @param array  $response
+     * @param int    $code
+     */
+    public function addlog(string $module, string $uri, array $request, array $response, int $code)
     {
         $logger      = new Logger($this->config->get('restapi.log_channel'));
         $file_name   = $this->config->get('restapi.log_file');
@@ -195,7 +297,16 @@ class Restapi
         $logger->addInfo($module);
     }
 
-    public function checkServer($request)
+    /**
+     * @date   2019/1/4
+     * @author <zhufengwei@100tal.com>
+     *
+     * @param \Illuminate\Http\Request $request
+     *
+     * @return bool
+     */
+    public function checkServer(\Illuminate\Http\Request $request)
+    : bool
     {
         $inputs = $request->all();
         $path   = $request->path();
