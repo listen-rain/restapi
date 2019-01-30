7a448af6b6b71bb1395c833aac42da4440ec8fbe
zhufengwei <zhufengwei@aliyun.com>
2019-01-30 17:17:26 +0800

    add callback trait

--- src/Restapi.php
+++ src/Restapi.php
@@ -6,12 +6,13 @@ use GuzzleHttp\Client;
 use GuzzleHttp\Exception\RequestException;
 use GuzzleHttp\Psr7\Request;
 use Illuminate\Contracts\Config\Repository;
-use Listen\LogCollector\LogCollector;
-use Listen\LogCollector\Logger;
-use Listen\Restapi\Exceptions\RestapiException;
 
 class Restapi
 {
+    use CallbackTrait {
+        CallbackTrait::__construct as private callbackConstruct;
+    }
+
     const BASENAME = 'restapi.';
 
     /**
@@ -49,91 +50,7 @@ class Restapi
                 'http_errors'     => true,
             ]);
 
-        $this->setLogger();
-        $this->pushExceptionCallback('logError', function ($module, $message, $code, $otherParams) {
-            static::$logger->restapiError(compact('module', 'message', 'code', 'otherParams'));
-        });
-    }
-
-    /**
-     * @date   2019/1/30
-     * @author <zhufengwei@aliyun.com>
-     *
-     * @param string $name
-     *
-     * @return $this
-     */
-    public function setLogger(string $name = 'restapi')
-    {
-        if (!static::$logger instanceof LogCollector) {
-            $mlogger = app(Logger::class, [$name])
-                ->setChannel($this->config->get('restapi.log_channel'))
-                ->setFile($this->config->get('restapi.log_file'))
-                ->setMode($this->config->get('restapi.log_mode'))
-                ->make();
-
-            static::$logger = app(LogCollector::class)
-                ->setBaseInfo('restapi', 'default')
-                ->addLogger($name, $mlogger);
-        }
-
-        return $this;
-    }
-
-    /**
-     * @date   2019/1/30
-     * @author <zhufengwei@aliyun.com>
-     *
-     * @param  string  $name
-     * @param \Closure $closure
-     *
-     * @return $this
-     */
-    public function pushExceptionCallback(string $name, \Closure $closure)
-    {
-        $this->exceptionCallbacks[$name] = \Closure::bind($closure, $this);
-
-        return $this;
-    }
-
-    /**
-     * @date   2019/1/30
-     * @author <zhufengwei@aliyun.com>
-     *
-     * @param string $name
-     *
-     * @return $this
-     * @throws \Listen\Restapi\Exceptions\RestapiException
-     */
-    public function popExceptionCallback(string $name)
-    {
-        if (!in_array($name, array_keys($this->exceptionCallbacks))) {
-            throw new RestapiException('Callback does\'t Exsits !');
-        }
-
-        unset($this->exceptionCallbacks[$name]);
-
-        return $this;
-    }
-
-    /**
-     * @date   2019/1/30
-     * @author <zhufengwei@aliyun.com>
-     *
-     * @param string $module
-     * @param string $message
-     * @param int    $code
-     * @param array  $otherParams
-     *
-     * @return $this
-     */
-    public function applyCallback(string $module, string $message, int $code, array $otherParams = [])
-    {
-        foreach ($this->exceptionCallbacks as $callback) {
-            $callback($module, $message, $code, $otherParams);
-        }
-
-        return $this;
+        $this->callbackConstruct();
     }
 
     /**
